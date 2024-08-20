5132dcbc6306428e3d29aa56a7b3b52684ef57a7
mysoyul <vega2k_@naver.com>
2024-08-20 22:59:20 +0900

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,38 @@
-import { defineConfig, loadEnv  } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig({
+  base: './',
+  plugins: [react()],
 
-  const API_URL = `${env.VITE_APIURL ?? 'http://localhost:8080'}`;
-  const PORT = `${env.VITE_PORT ?? '3000'}`;
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+          target: 'http://employee-api-svc:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+    }
+  }
+})
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
 
-  return {
-    server: {
-      proxy: {
-        '/api': API_URL,
-      },
-      port: PORT,
-    },
-    build: {
-      outDir: 'public',
-    },
-    plugins: [react()],
-  };
-});
+//   const API_URL = `${env.VITE_APIURL ?? 'http://localhost:8080'}`;
+//   const PORT = `${env.VITE_PORT ?? '3000'}`;
+
+//   return {
+//     server: {
+//       proxy: {
+//         '/api': API_URL,
+//       },
+//       port: PORT,
+//     },
+//     build: {
+//       outDir: 'public',
+//     },
+//     plugins: [react()],
+//   };
+// });
