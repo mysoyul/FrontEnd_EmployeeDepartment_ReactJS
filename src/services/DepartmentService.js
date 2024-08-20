9c4b9958bd42b69045cb68213911e101e104f651
mysoyul <vega2k_@naver.com>
2024-08-20 23:18:33 +0900

    url all update

--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const REST_API_BASE_URL =  import.meta.env.VITE_APIURL;
 
-const DEPARTMENT_REST_API_URL = `${REST_API_BASE_URL}/departments`
+const DEPARTMENT_REST_API_URL = `${REST_API_BASE_URL}/api/departments`
+console.log(`DEPARTMENT_REST_API_URL = ${DEPARTMENT_REST_API_URL}`)
 //'http://localhost:8080/api/departments'
 
 export const getAllDepartments = () => axios.get(DEPARTMENT_REST_API_URL);
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -2,18 +2,16 @@ import axios from "axios";
 
 const REST_API_BASE_URL =  import.meta.env.VITE_APIURL; //http://localhost:8080
 
-const REST_API_URL = `${REST_API_BASE_URL}/employees`
+const REST_API_URL = `${REST_API_BASE_URL}/api/employees`
+console.log(`REST_API_URL = ${REST_API_URL}`)
 //'http://localhost:8080/api/employees';
 
-//export const listEmployees = () => axios.get(REST_API_BASE_URL);
-//export const listEmployees = () => axios.get(`${REST_API_URL}/departments`);
-export const listEmployees = () => axios.get(`/api/employees/departments`);
+export const listEmployees = () => axios.get(`${REST_API_URL}/departments`);
 
+export const createEmployee = (employee) => axios.post(`${REST_API_URL}`, employee);
 
-export const createEmployee = (employee) => axios.post(REST_API_URL, employee);
+export const getEmployee = (employeeId) => axios.get(`${REST_API_URL}/${employeeId}` );
 
-export const getEmployee = (employeeId) => axios.get(REST_API_URL + '/' + employeeId);
+export const updateEmployee = (employeeId, employee) => axios.put(`${REST_API_URL}/${employeeId}`, employee);
 
-export const updateEmployee = (employeeId, employee) => axios.put(REST_API_URL + '/' + employeeId, employee);
-
-export const deleteEmployee = (employeeId) => axios.delete(REST_API_URL + '/' + employeeId);
+export const deleteEmployee = (employeeId) => axios.delete(`${REST_API_URL}/${employeeId}`);
